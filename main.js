3a66362073ebcfb6a1f708ea93124b9ea37125be
ape <kgonzale@cbu.edu>
2019-04-06 14:06:49 -0500

    saving before deploying

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
-$(document).ready(function(){
-    $('.carousel-daddy').slick({
-        // slidesToShow: 1,
-        // slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-        arrows: false,
-        dots: true,
-        autoplay: true
-      });
+$(document).ready(function() {
+  const stickyNavTop = $(".topnav").offset().top;
+
+  const stickyNav = () => {
+    const scrollTop = $(window).scrollTop();
+
+    scrollTop > stickyNavTop
+      ? $(".topnav").addClass("sticky")
+      : $(".topnav").removeClass("sticky");
+  };
+
+  stickyNav();
+
+  $(window).scroll(function() {
+    stickyNav();
   });
 
-
-
-
-
-
+  $(".carousel-daddy").slick({
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+    autoplay: true
+  });
+});
