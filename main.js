1e99843ea2dd064171aa61045472ab91c156b3e1
unknown <kgonzale@cbu.edu>
2019-04-05 20:42:25 -0500

    slock working

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
-    $('#carousel-daddy').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
+    $('.carousel-daddy').slick({
+        // slidesToShow: 1,
+        // slidesToScroll: 1,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+        autoplay: true
       });
   });
 
